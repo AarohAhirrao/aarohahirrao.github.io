ecb386f3b42f22c7b7e5f8ce2d081505376066a1
AarohAhirrao <aaroh403@gmail.com>
2025-04-26 20:13:40 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = 'About Me';
+require 'templates/header.php';
+?>
+<section>
+  <h2>About Me</h2>
+  <p>Write a short bio here—your skills, background, passions, etc.</p>
+</section>
+<?php require 'templates/footer.php'; ?>
