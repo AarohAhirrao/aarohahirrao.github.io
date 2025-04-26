659e07bf762376e1463103a522c68b9c539451d2
AarohAhirrao <aaroh403@gmail.com>
2025-04-26 20:13:19 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = 'Home';
+require 'templates/header.php';
+?>
+<section>
+  <h2>Welcome!</h2>
+  <p>Hi, I’m Aaroh. I’m a programmer. Welcome to my personal site.</p>
+</section>
+<?php require 'templates/footer.php'; ?>
+
